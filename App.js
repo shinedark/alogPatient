f13daf4b9e93aa4ad030ece2ebc5383af49638f7
shinedark <mide0303@gmail.com>
2019-01-26 20:54:35 -0600

    i broke something

--- App.js
+++ App.js
@@ -4,7 +4,48 @@ import Log from './components/Log';
 import Events from './components/Events';
 import Progress from './components/Progress';
 
-export default class TabsAdvancedExample extends Component {
+
+import { withAuthenticator } from 'aws-amplify-react-native';
+import { Auth } from 'aws-amplify';
+import Amplify from '@aws-amplify/core';
+import config from './aws-exports';
+Amplify.configure(config)
+
+class App extends React.Component {
+  state = {
+    username: '',
+    password: '',
+    phone_number: '',
+    email: '',
+    authCode: '',
+    user: {}
+  }
+  async signUp() {
+    const { username, password, email, phone_number } = this.state
+    await Auth.signUp({
+      username,
+      password,
+      attributes: { email, phone_number }
+    })
+    console.log('sign up successful!')
+  }
+  async confirmSignUp() {
+    const { username, authCode } = this.state
+    await Auth.configSignignUp(username, authCode)
+    console.log('confirm sign up successful!')
+  }
+  async signIn() {
+    const { username, password  } = this.state
+    const user = await Auth.signIn(username, password)
+    this.setState({ user })
+    console.log('sign in successful!')
+  }
+  async confirmSignIn() {
+    const { user, authCode } = this.state
+    await Auth.configSignignIn(user, authCode)
+    console.log('user now successfully signed in to the app!!')
+  }
+  
   render() {
     return (
       <Container>
@@ -28,3 +69,6 @@ export default class TabsAdvancedExample extends Component {
     );
   }
 }
+
+export default withAuthenticator(App, { includeGreetings: true });
+
